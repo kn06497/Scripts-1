0a8e9c447df5ac4a45b7b8c131c95cb8b9cf8af9
Grace5921 <androidlover5842@gmail.com>
2016-09-23 16:56:36 +0530

    dtb: use c lan to fix dtb issue

---
+++ unpack-boot/tools/mkboot.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+void load_menu(void);
+void dtb(void);
+void non_dtb(void);
+char *build = "build";
+int main(int argc, char** argv)
+{
+    load_menu();
+    return 0;
+}
+ 
+void load_menu(void)
+{
+    int choice;
+ 
+    do
+    {
+   system("echo 'UNPACK-BOOT'");
+        printf("1. Without dtb\n");
+	printf("2. With dtb\n");
+        printf("3. Exit\n");
+        scanf("%d",&choice);
+ 
+        switch(choice)
+        {
+            case 1: non_dtb();
+                break;
+            case 2: dtb();
+		break;
+            case 3: printf("Quitting program!\n");
+		unlink(build);
+                exit(0);
+                break;
+            default: printf("Invalid choice!\n");
+                break;
+        }
+ 
+    } while (choice != 3);
+ 
+}
+
+void non_dtb(void)
+{
+    		system("chmod 775 *");
+    		system("cp tools/non-dtb non-dtb");
+    		system(" ./non-dtb ;rm non-dtb");
+		unlink(build);
+                exit(0);
+}     
+ 
+void dtb(void)
+{
+    		system("chmod 775 tools/*");
+    		system("cp tools/dtb dtb");
+    		system("./dtb ;rm dtb");
+		unlink(build);
+                exit(0);
+}
